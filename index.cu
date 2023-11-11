356c91382ca520f07c310bd81bd0c769e62df98e
joaoprcf <joaoprcf@ua.pt>
2023-11-11 23:41:58 +0000

    First commit, neural netowrk base and tests

---
+++ index.cu
@@ -0,0 +1,26 @@
+#include "src/inline_nn.h"
+
+int main()
+{
+
+    Input input1(5);
+    Dense dense1(&input1, 2);
+    Dense dense2(&dense1, 2);
+    Concatenate ct({&dense1, &dense2});
+    Dense dense3(&ct, 2);
+    NeuralNetwork nn(&input1, &dense3);
+
+    dense1.weights[6] = 1;
+    dense2.weights[4] = 1;
+
+    dense3.weights[1] = 10;
+    dense3.weights[3] = -7;
+    dense3.weights[6] = -10;
+    dense3.weights[8] = 7;
+    vector<float> data_in = {1.0, 1.0, 1.0, 1.0, 1.0};
+    float *result = nn.FeedForwardSingle(data_in.data());
+    assert(3.0f == result[0]);
+    assert(-3.0f == result[1]);
+    // CUDA code goes here
+    return 0;
+}
