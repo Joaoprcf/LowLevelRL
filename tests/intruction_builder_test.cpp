e99cd4c74a0e25d088158277282690d54133ad62
joaoprcf <joaoprcf@ua.pt>
2023-11-12 16:00:43 +0000

    Adapted PipelineBuilder to have GPU compability, added more tests

--- tests/intruction_builder_test.cpp
+++ tests/intruction_builder_test.cpp
@@ -10,10 +10,10 @@ TEST_CASE("PipelineBuilder Initialization")
 
     PipelineBuilder builder(&nn);
 
-    REQUIRE(builder.instructions.size() == nn.fastExecution.size());
-    REQUIRE(builder.input_sizes.size() == nn.inputs.size());
-    REQUIRE(builder.output_sizes.size() == nn.outputs.size());
-    REQUIRE(builder.output_locations.size() == nn.outputLocations.size());
+    REQUIRE(builder.num_instructions == nn.fastExecution.size());
+    REQUIRE(builder.num_inputs == nn.inputs.size());
+    REQUIRE(builder.num_outputs == nn.outputs.size());
+    REQUIRE(builder.num_outputs == nn.outputLocations.size());
     REQUIRE(builder.weight_size == nn.weights.size());
 }
 
@@ -29,10 +29,10 @@ TEST_CASE("PipelineBuilder Initialization Multi Input Output")
 
     PipelineBuilder builder(&nn);
 
-    REQUIRE(builder.instructions.size() == nn.fastExecution.size());
-    REQUIRE(builder.input_sizes.size() == nn.inputs.size());
-    REQUIRE(builder.output_sizes.size() == nn.outputs.size());
-    REQUIRE(builder.output_locations.size() == nn.outputLocations.size());
+    REQUIRE(builder.num_instructions == nn.fastExecution.size());
+    REQUIRE(builder.num_inputs == nn.inputs.size());
+    REQUIRE(builder.num_outputs == nn.outputs.size());
+    REQUIRE(builder.num_outputs == nn.outputLocations.size());
     REQUIRE(builder.weight_size == nn.weights.size());
 }
 
@@ -44,7 +44,7 @@ TEST_CASE("Instruction Conversion")
 
     PipelineBuilder builder(&nn);
 
-    for (size_t i = 0; i < builder.instructions.size(); ++i)
+    for (size_t i = 0; i < builder.num_instructions; ++i)
     {
         REQUIRE(builder.instructions[i].op == nn.fastExecution[i].op);
         REQUIRE(builder.instructions[i].size_in == nn.fastExecution[i].size_in);
@@ -60,5 +60,163 @@ TEST_CASE("Output Location Mapping")
 
     PipelineBuilder builder(&nn);
 
-    REQUIRE(builder.output_locations[0] == input1.size_out);
+    REQUIRE(builder.outputLocations[0] == input1.size_out);
+}
+
+TEST_CASE("NeuralNetwork FeedForwardSingle Test")
+{
+    Input input1(5);
+    Dense dense1(&input1, 2);
+    Dense dense2(&dense1, 2);
+    Concatenate ct({&dense1, &dense2});
+    Dense dense3(&ct, 2);
+    NeuralNetwork nn(&input1, &dense3);
+
+    dense1.weights[6] = 1;
+    dense2.weights[4] = 1;
+
+    dense3.weights[1] = 10;
+    dense3.weights[3] = -7;
+    dense3.weights[6] = -10;
+    dense3.weights[8] = 7;
+
+    std::vector<float> data_in = {1.0, 1.0, 1.0, 1.0, 1.0};
+
+    PipelineBuilder builder(&nn);
+    float *customDatastream = new float[nn.datastream.size()];
+    float *result = new float[nn.outputs[0]->size_out];
+    builder.init(customDatastream, nn.weights.data());
+    builder.FeedForwardSingle(data_in.data(), customDatastream, result);
+
+    REQUIRE(result[0] == Approx(3.0f));
+    REQUIRE(result[1] == Approx(-3.0f));
+}
+
+TEST_CASE("PipelineBuilder Calculate Memory Required for Neural Network")
+{
+    // Setup the neural network structure
+    Input input1(5);
+    Dense dense1(&input1, 2);
+    Dense dense2(&dense1, 2);
+    Concatenate ct({&dense1, &dense2});
+    Dense dense3(&ct, 2);
+    NeuralNetwork nn(&input1, &dense3);
+
+    // Create a PipelineBuilder instance
+    PipelineBuilder builder(&nn);
+
+    // Manually calculate the expected size
+    size_t expectedSize = 5 * sizeof(RecoverableInstruction); // For 5 instructions
+    expectedSize += 3 * sizeof(size_t);                       // For one input size, one output size, and one output location
+
+    // Get the calculated size from the function
+    size_t calculatedSize = builder.calculateMemoryRequired();
+
+    // Check if the calculated size matches the expected size
+    REQUIRE(calculatedSize == expectedSize);
+}
+
+TEST_CASE("PipelineBuilder Serialize Memory Test")
+{
+    // Setup the neural network structure
+    Input input1(5);
+    Dense dense1(&input1, 2);
+    Dense dense2(&dense1, 2);
+    Concatenate ct({&dense1, &dense2});
+    Dense dense3(&ct, 2);
+    NeuralNetwork nn(&input1, &dense3);
+
+    // Create a PipelineBuilder instance
+    PipelineBuilder builder(&nn);
+
+    // Allocate buffer based on the calculated size
+    size_t bufferSize = builder.calculateMemoryRequired();
+    void *buffer = malloc(bufferSize);
+
+    // Serialize data into the buffer
+    builder.serializeMemory(buffer);
+
+    // Cast buffer to char* for byte-wise comparison
+    char *serializedData = static_cast<char *>(buffer);
+
+    // Verify contents of the buffer
+    // (This part depends on the internal structure of PipelineBuilder and its components)
+    // Example:
+    // Compare first part of the buffer with the instructions
+    char *expectedInstructions = reinterpret_cast<char *>(builder.instructions);
+    for (size_t i = 0; i < 5 * sizeof(RecoverableInstruction); ++i)
+    {
+        REQUIRE(serializedData[i] == expectedInstructions[i]);
+    }
+
+    size_t offset = 5 * sizeof(RecoverableInstruction); // Adjust based on actual number of instructions
+
+    // Compare inputSizes
+    char *expectedInputSizes = reinterpret_cast<char *>(builder.inputSizes);
+    for (size_t i = 0; i < sizeof(size_t); ++i)
+    {
+        REQUIRE(serializedData[offset + i] == expectedInputSizes[i]);
+    }
+    offset += sizeof(size_t);
+
+    // Compare outputSizes
+    char *expectedOutputSizes = reinterpret_cast<char *>(builder.outputSizes);
+    for (size_t i = 0; i < sizeof(size_t); ++i)
+    {
+        REQUIRE(serializedData[offset + i] == expectedOutputSizes[i]);
+    }
+    offset += sizeof(size_t);
+
+    // Compare outputLocations
+    char *expectedOutputLocations = reinterpret_cast<char *>(builder.outputLocations);
+    for (size_t i = 0; i < sizeof(size_t); ++i)
+    {
+        REQUIRE(serializedData[offset + i] == expectedOutputLocations[i]);
+    }
+
+    // Free the allocated buffer
+    free(buffer);
+}
+
+TEST_CASE("PipelineBuilder Serialize and Deserialize Test")
+{
+    // Setup the neural network structure
+    Input input1(5);
+    Input input2(10);
+    Concatenate ct({&input1, &input2});
+    Dense dense1(&ct, 3);
+    Dense dense2(&ct, 4);
+
+    NeuralNetwork nn({&input1, &input2}, {&dense1, &dense2});
+
+    // Create a PipelineBuilder instance
+    PipelineBuilder originalBuilder(&nn);
+
+    // Serialize originalBuilder into a buffer
+    size_t bufferSize = originalBuilder.calculateMemoryRequired();
+    void *buffer = malloc(bufferSize);
+    originalBuilder.serializeMemory(buffer);
+
+    PipelineBuilder newBuilder = originalBuilder;
+    newBuilder.unserializeMemory(buffer);
+
+    REQUIRE(memcmp(newBuilder.instructions, originalBuilder.instructions,
+                   originalBuilder.num_instructions * sizeof(RecoverableInstruction)) == 0);
+
+    // Compare inputSizes, outputSizes, and outputLocations
+    for (size_t i = 0; i < originalBuilder.num_inputs; ++i)
+    {
+        REQUIRE(newBuilder.inputSizes[i] == originalBuilder.inputSizes[i]);
+    }
+    for (size_t i = 0; i < originalBuilder.num_outputs; ++i)
+    {
+        REQUIRE(newBuilder.outputSizes[i] == originalBuilder.outputSizes[i]);
+    }
+    for (size_t i = 0; i < originalBuilder.num_outputs; ++i)
+    {
+        REQUIRE(newBuilder.outputLocations[i] == originalBuilder.outputLocations[i]);
+    }
+
+    // Free the allocated buffer
+    free(buffer);
 }
