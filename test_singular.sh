9ac5100c63e6b5b46744507af205bca78af05a32
joaoprcf <joaoprcf@ua.pt>
2023-11-14 15:45:20 +0000

    Added script to run singular tests

---
+++ test_singular.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if a test name was provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <test_name>"
+    exit 1
+fi
+
+TEST_NAME=$1
+BUILD_DIR=./build
+TEST_DIR=./tests
+CXX=g++
+CXXFLAGS="-pthread -lstdc++ -std=gnu++17 -lrt -ldl -lm -I./src -I./tests -I/usr/local/cuda/include"
+
+# Create the build folder
+mkdir -p build
+
+# Compile the test
+echo "Compiling $TEST_NAME..."
+$CXX $CXXFLAGS $TEST_DIR/${TEST_NAME}_test.cpp -o $BUILD_DIR/${TEST_NAME}_test
+
+# Check if compilation was successful
+if [ $? -ne 0 ]; then
+    echo "Compilation failed"
+    exit 1
+fi
+
+# Run the test
+echo "Running $TEST_NAME..."
+$BUILD_DIR/${TEST_NAME}_test
