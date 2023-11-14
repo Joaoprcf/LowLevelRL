218e69992b0c42c71b6aefa4aea40f37bcc8d472
joaoprcf <joaoprcf@ua.pt>
2023-11-14 00:27:53 +0000

    Updated code for instructions, added activation to dense layer

--- tests/inline_nn_test.cpp
+++ tests/inline_nn_test.cpp
@@ -27,6 +27,51 @@ TEST_CASE("NeuralNetwork FeedForwardSingle Test")
     REQUIRE(nn.fastExecution.size() == 5);
 }
 
+TEST_CASE("NeuralNetwork FeedForwardSingle Test Activations")
+{
+    Input input1(5);
+    Dense dense1(&input1, 2);
+    Dense dense2(&dense1, 2);
+    Concatenate ct({&dense1, &dense2});
+    Dense dense3(&ct, 2, ACTIVATION_RELU);
+    NeuralNetwork nn(&input1, &dense3);
+
+    dense1.weights[6] = 1;
+    dense2.weights[4] = 1;
+
+    dense3.weights[1] = 10;
+    dense3.weights[3] = -7;
+    dense3.weights[6] = -10;
+    dense3.weights[8] = 7;
+
+    std::vector<float> data_in = {1.0, 1.0, 1.0, 1.0, 1.0};
+    float *result = nn.FeedForwardSingle(data_in.data());
+
+    REQUIRE(result[0] == Approx(3.0f));
+    REQUIRE(result[1] == Approx(-0.0f));
+    REQUIRE(nn.fastExecution.size() == 6);
+
+    dense3.activation = ACTIVATION_TANH;
+
+    nn.BuildFastExecutionGraph(false);
+
+    result = nn.FeedForwardSingle(data_in.data());
+
+    REQUIRE(result[0] == Approx(0.995054f));
+    REQUIRE(result[1] == Approx(-0.995054f));
+    REQUIRE(nn.fastExecution.size() == 6);
+
+    dense3.activation = ACTIVATION_SIGMOID;
+
+    nn.BuildFastExecutionGraph(false);
+
+    result = nn.FeedForwardSingle(data_in.data());
+
+    REQUIRE(result[0] == Approx(0.952574f));
+    REQUIRE(result[1] == Approx(0.0474259f));
+    REQUIRE(nn.fastExecution.size() == 6);
+}
+
 TEST_CASE("NeuralNetwork initialized with usingOwnWeights as false - Simple Setup")
 {
     Input input(3);
