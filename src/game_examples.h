320f729100ebd254325f7891c1ac41c7b44132e5
joaoprcf <joaoprcf@ua.pt>
2023-11-23 18:17:12 +0100

    Added GuessGameHard game example

--- src/game_examples.h
+++ src/game_examples.h
@@ -63,3 +63,58 @@ struct GuessGame
         missing_steps -= 1;
     }
 };
+
+struct GuessGameHard
+{
+    float reward = 0;
+
+    int missing_steps = 20;
+
+    int action_space = 2;
+
+    int observation_space = 5;
+
+    uint32_t seed; // Seed for random number generation
+
+    float values[5];
+
+    __host__ __device__ GuessGameHard() : reward(0), seed(12345) {}
+
+    __host__ __device__ GuessGameHard(uint32_t initSeed) : reward(0), seed(initSeed) {}
+
+    __host__ __device__ void generateValues()
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            // Generate a value between 0 and 1, then scale to [-1, 1]
+            float randVal = static_cast<float>(fastRand(seed, 500)) / 500;
+            values[i] = randVal * 2 - 1; // Scale and shift to [-1, 1]
+        }
+    }
+
+    __host__ __device__ void reset(float *observation)
+    {
+        reward = 0;
+        missing_steps = 20;
+        generateValues();
+        for (int i = 0; i < observation_space; ++i)
+        {
+            observation[i] = values[i];
+        }
+    }
+
+    __host__ __device__ void step(float *action, float *observation)
+    {
+        float expected1 = values[0] > values[1] ? values[0] - values[1] * 0.5f : values[2] + values[3] * 2.0f;
+        float expected2 = values[4] > values[3] ? values[3] - values[2] * 0.5f : values[1] + values[0] * 2.0f;
+
+        reward += (1.0f / (abs(expected1 - action[0]) + abs(expected2 - action[1]) + 0.01f));
+
+        generateValues();
+        for (int i = 0; i < observation_space; ++i)
+        {
+            observation[i] = values[i];
+        }
+        missing_steps -= 1;
+    }
+};
