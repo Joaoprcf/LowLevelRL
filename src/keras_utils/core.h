ff0f9dbd55656af3b1e33fd3abc72f3a63ddd721
joaoprcf <joaoprcf@ua.pt>
2023-12-27 03:55:33 +0000

    Added keras utils logic and correspondent tests

---
+++ src/keras_utils/core.h
@@ -0,0 +1,36 @@
+
+
+void divideWeightsAndBias(float *dst, const float *origin, size_t size_in, size_t size_out)
+{
+    size_t total_elements = (size_in + 1) * size_out;
+    size_t weights_count = size_in * size_out;
+
+    size_t idx = 0;
+    for (size_t i = 0; i < size_out; i++)
+    {
+        for (size_t j = 0; j < size_in; j++, idx++)
+        {
+            dst[idx] = origin[idx + i];
+        }
+        dst[weights_count + i] = origin[idx + i];
+    }
+}
+
+void mergeWeightsAndBias(float *dst, const float *origin, size_t size_in, size_t size_out)
+{
+    size_t total_elements = (size_in + 1) * size_out;
+    size_t weights_count = size_in * size_out;
+
+    size_t idx = 0;
+    for (size_t i = 0; i < size_out; i++)
+    {
+        for (size_t j = 0; j < size_in; j++, idx++)
+        {
+            dst[idx + i] = origin[idx];
+        }
+    }
+    for (size_t biax_idx = size_in; idx < total_elements; idx++, biax_idx += size_in + 1)
+    {
+        dst[biax_idx] = origin[idx];
+    }
+}
---
+++ tests/keras_utils_test.cpp
@@ -0,0 +1,82 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../src/keras_utils/core.h"
+
+TEST_CASE("divideWeightsAndBias Functionality 2x3")
+{
+    // Test with small sizes
+    float origin[] = {1, 2, 0.5, 3, 4, 0.7, 5, 6, 0.9};
+    float expected[] = {1, 2, 3, 4, 5, 6, 0.5, 0.7, 0.9};
+    float dst[9];
+    divideWeightsAndBias(dst, origin, 2, 3);
+    REQUIRE(memcmp(dst, expected, 9 * sizeof(float)) == 0);
+}
+
+TEST_CASE("divideWeightsAndBias Functionality 3x2")
+{
+    // Test with small sizes
+    float origin[] = {1, 2, 3, 0.5, 4, 5, 6, 0.7};
+    float expected[] = {1, 2, 3, 4, 5, 6, 0.5, 0.7};
+    float dst[8];
+    divideWeightsAndBias(dst, origin, 3, 2);
+    REQUIRE(memcmp(dst, expected, 8 * sizeof(float)) == 0);
+}
+
+TEST_CASE("mergeWeightsAndBias Functionality 2x3")
+{
+    // Test with small sizes
+    float origin[] = {1, 2, 3, 4, 5, 6, 0.5, 0.7, 0.9};
+    float expected[] = {1, 2, 0.5, 3, 4, 0.7, 5, 6, 0.9};
+    float dst[9];
+    mergeWeightsAndBias(dst, origin, 2, 3);
+    REQUIRE(memcmp(dst, expected, 9 * sizeof(float)) == 0);
+}
+
+TEST_CASE("mergeWeightsAndBias Functionality 3x2")
+{
+    // Test with small sizes
+    float origin[] = {1, 2, 3, 4, 5, 6, 0.5, 0.7};
+    float expected[] = {1, 2, 3, 0.5, 4, 5, 6, 0.7};
+    float dst[8];
+    mergeWeightsAndBias(dst, origin, 3, 2);
+    REQUIRE(memcmp(dst, expected, 8 * sizeof(float)) == 0);
+}
+
+TEST_CASE("Apply Divide and Merge Multiple Times")
+{
+    const size_t size_in = 3;
+    const size_t size_out = 2;
+    float original[] = {1, 2, 3, 0.5, 4, 5, 6, 0.7};
+    float expected_divide[] = {1, 2, 3, 4, 5, 6, 0.5, 0.7};
+    float expected_merge[] = {1, 2, 3, 6, 0.5, 4, 5, 0.7};
+    float intermediate[8];
+    float result[8];
+
+    // First application of divide and merge
+    divideWeightsAndBias(intermediate, original, size_in, size_out);
+    mergeWeightsAndBias(result, intermediate, size_in, size_out);
+
+    REQUIRE(memcmp(intermediate, expected_divide, 8 * sizeof(float)) == 0);
+    REQUIRE(memcmp(result, original, 8 * sizeof(float)) == 0);
+
+    // Second application of divide and merge
+    divideWeightsAndBias(intermediate, result, size_in, size_out);
+    mergeWeightsAndBias(result, intermediate, size_in, size_out);
+
+    REQUIRE(memcmp(intermediate, expected_divide, 8 * sizeof(float)) == 0);
+    REQUIRE(memcmp(result, original, 8 * sizeof(float)) == 0);
+
+    // First application of merge and divide
+    mergeWeightsAndBias(intermediate, original, size_in, size_out);
+    divideWeightsAndBias(result, intermediate, size_in, size_out);
+
+    REQUIRE(memcmp(intermediate, expected_merge, 8 * sizeof(float)) == 0);
+    REQUIRE(memcmp(result, original, 8 * sizeof(float)) == 0);
+
+    // Second application of merge and divide
+    mergeWeightsAndBias(intermediate, result, size_in, size_out);
+    divideWeightsAndBias(result, intermediate, size_in, size_out);
+
+    REQUIRE(memcmp(intermediate, expected_merge, 8 * sizeof(float)) == 0);
+    REQUIRE(memcmp(result, original, 8 * sizeof(float)) == 0);
+}
