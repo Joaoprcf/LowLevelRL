fc3b063445a5dbcfd5fea980ca210c98055a0a0e
joaoprcf <joaoprcf@ua.pt>
2023-12-30 21:14:33 +0000

    Added support for training weights that require initialization, more tests and also added EarlyStopping to restore the best weights on keras training

--- keras_utils/train.py
+++ keras_utils/train.py
@@ -9,7 +9,7 @@ outputs = []
 model = Model(inputs=fake_input, outputs=Dense(2)(fake_input))
 
 #$SCRIPT_START
-
+from keras.callbacks import EarlyStopping
 
 def preprocess_dynamic_data(data_stream, data_lengths):
     # Calculate the total length of the data segments
@@ -57,22 +57,22 @@ with open('data_buffer/temp_data_y.bin', 'rb') as f:
 data_y = preprocess_dynamic_data(np.frombuffer(data_y, dtype=np.float32), [output_layer.shape[1] for output_layer in outputs])
 
 # print(data_y)
-
-for layer in all_layers:
-    if hasattr(layer, 'weights') and len(layer.weights) > 0:
-      weights_size = layer.weights[0].shape[0] * layer.weights[0].shape[1]
-      layer_weights = weights[:weights_size]
-      layer_weights = layer_weights.reshape(layer.weights[0].shape)
-      layer.set_weights([layer_weights, np.zeros(layer.weights[1].shape)])
-      weights = weights[weights_size:]
-    else:
-       continue
-    if hasattr(layer, 'bias'):
-      layer_bias = weights[:layer.bias.shape[0]]
-      layer.set_weights([layer.get_weights()[0], layer_bias])
-      weights = weights[layer.bias.shape[0]:]
-      
-assert(len(weights) == 0)
+if np.any(weights != 0):
+    for layer in all_layers:
+        if hasattr(layer, 'weights') and len(layer.weights) > 0:
+            weights_size = layer.weights[0].shape[0] * layer.weights[0].shape[1]
+            layer_weights = weights[:weights_size]
+            layer_weights = layer_weights.reshape(layer.weights[0].shape)
+            layer.set_weights([layer_weights, np.zeros(layer.weights[1].shape)])
+            weights = weights[weights_size:]
+        else:
+            continue
+        if hasattr(layer, 'bias'):
+            layer_bias = weights[:layer.bias.shape[0]]
+            layer.set_weights([layer.get_weights()[0], layer_bias])
+            weights = weights[layer.bias.shape[0]:]
+        
+    assert(len(weights) == 0)
 
 epochs=20 
 batch_size=1 
@@ -80,7 +80,21 @@ batch_size=1
 #$EPOCHS_PLACEHOLDER
 #$BATCH_SIZE_PLACEHOLDER
 
-model.fit(data_x, data_y, epochs=epochs, batch_size=batch_size)
+early_stopping_callback = EarlyStopping(
+    monitor='loss',  # monitor training loss
+    patience=epochs,     # number of epochs with no improvement after which training will be stopped
+    mode='min',      # since we want to minimize loss
+    verbose=1,
+    restore_best_weights=True  # restores model weights from the epoch with the best value of the monitored quantity
+)
+
+model.fit(
+    data_x, 
+    data_y, 
+    epochs=epochs, 
+    batch_size=batch_size, 
+    callbacks=early_stopping_callback
+)
 
 # Save weights to a binary file
 weights = []
--- tests/backpropagation_test.cpp
+++ tests/backpropagation_test.cpp
@@ -10,7 +10,7 @@ TEST_CASE("Linear regression")
     Dense output(&input1, 1);
     Model nn(&input1, &output);
 
-    nn.compile("Adam(learning_rate=0.1)");
+    nn.compile("Adam(learning_rate=0.2)");
 
     const size_t data_samples = 10;
 
@@ -31,14 +31,10 @@ TEST_CASE("Linear regression")
         printf("result[%lu]: %f\n", i, result[0]);
     }
 
-    nn.fit(nn.weights, nn.weights, data_x, data_y, data_samples, 50, 2);
+    nn.fit(nn.weights, nn.weights, data_x, data_y, data_samples, 100, 2);
 
     REQUIRE(abs(nn.weights[0] - m) < 0.01f);
     REQUIRE(abs(nn.weights[1] - b) < 0.01f);
-    for (size_t i = 0; i < nn.weights_size; i++)
-    {
-        printf("w[%lu]: %f\n", i, nn.weights[i]);
-    }
 
     for (size_t i = 0; i < 6; i++)
     {
@@ -48,7 +44,7 @@ TEST_CASE("Linear regression")
     }
 }
 
-TEST_CASE("Linear regression with 2 inputs and 2 outptus")
+TEST_CASE("Linear regression with 2 inputs and 2 outputs")
 {
     Input input1(2);
     Dense output(&input1, 2);
@@ -80,7 +76,7 @@ TEST_CASE("Linear regression with 2 inputs and 2 outptus")
         }
     }
 
-    nn.fit(nn.weights, nn.weights, data_x, data_y, prod_samples, 20, 1);
+    nn.fit(nn.weights, nn.weights, data_x, data_y, prod_samples, 100, 1);
     for (size_t i = 0; i < nn.weights_size; i++)
     {
         printf("w[%lu]: %f\n", i, nn.weights[i]);
@@ -116,23 +112,23 @@ TEST_CASE("Solve GuessGame with regression")
         for (int j = 0; j < 5; ++j)
         {
             // Generate a value between 0 and 1, then scale to [-1, 1]
-            float randVal = static_cast<float>(game::fastRand(seed, 500)) / 500;
+            float randVal = static_cast<float>(game::fastRand(seed, 501)) / 500;
             data_x_ptr[j] = randVal * 2 - 1; // Scale and shift to [-1, 1]
         }
         data_y_ptr[0] = data_x_ptr[0] - data_x_ptr[1] * 0.5f;
         data_y_ptr[1] = data_x_ptr[2] + data_x_ptr[3] * 2.0f;
     }
 
-    nn.fit(nn.weights, nn.weights, data_x, data_y, data_samples, 10, 4);
+    nn.fit(nn.weights, nn.weights, data_x, data_y, data_samples, 100, 4);
     for (size_t i = 0; i < nn.weights_size; i++)
     {
         printf("w[%lu]: %f\n", i, nn.weights[i]);
     }
 
-    REQUIRE(abs(nn.weights[0] - 1) < 0.01f);
-    REQUIRE(abs(nn.weights[1] + 0.5) < 0.01f);
-    REQUIRE(abs(nn.weights[8] - 1) < 0.01f);
-    REQUIRE(abs(nn.weights[9] - 2) < 0.01f);
+    REQUIRE(abs(nn.weights[0] - 1) < 0.05f);
+    REQUIRE(abs(nn.weights[1] + 0.5) < 0.05f);
+    REQUIRE(abs(nn.weights[8] - 1) < 0.05f);
+    REQUIRE(abs(nn.weights[9] - 2) < 0.05f);
 
     float observation[5];
     float reward = 0.0f;
@@ -148,4 +144,115 @@ TEST_CASE("Solve GuessGame with regression")
     }
     printf("Reward: %f\n", reward);
     REQUIRE(reward > 79500.0f); // Almost max points
+}
+
+TEST_CASE("Linear regression with 2 inputs, 2 outputs and 2 hidden layers")
+{
+    Input input1(2);
+    Dense middle1(&input1, 5);
+    Dense middle2(&middle1, 5);
+    Dense output(&middle2, 2);
+    Model nn(&input1, &output);
+
+    nn.compile("Adam(learning_rate=0.1)");
+
+    const size_t data_samples = 400;
+
+    float data_x[data_samples * 2] = {0};
+    float data_y[data_samples * 2] = {0};
+    float m11 = 1.5f;
+    float m12 = -1.5f;
+    float b1 = -1.0f;
+
+    float m21 = -2.5f;
+    float m22 = 0.5f;
+    float b2 = -0.5f;
+    uint32_t seed = 12345;
+    for (size_t i = 0; i < data_samples; i++)
+    {
+        float *data_x_ptr = data_x + i * 2;
+        float *data_y_ptr = data_y + i * 2;
+        for (int j = 0; j < 2; ++j)
+        {
+            // Generate a value between 0 and 1, then scale to [-2, 2]
+            float randVal = static_cast<float>(game::fastRand(seed, 1001)) / 1000;
+            data_x_ptr[j] = randVal * 4.0f - 2.0f; // Scale and shift to [-2, 2]
+        }
+        data_y_ptr[0] = data_x_ptr[0] * m11 + data_x_ptr[1] * m12 + b1;
+        data_y_ptr[1] = data_x_ptr[0] * m21 + data_x_ptr[1] * m22 + b2;
+    }
+
+    nn.fit(nn.weights, nn.weights, data_x, data_y, data_samples, 100, 4);
+    for (size_t i = 0; i < nn.weights_size; i++)
+    {
+        printf("w[%lu]: %f\n", i, nn.weights[i]);
+    }
+
+    for (size_t i = 0; i < data_samples; i++)
+    {
+        float *data_x_ptr = data_x + i * 2;
+        float *data_y_ptr = data_y + i * 2;
+        for (int j = 0; j < 2; ++j)
+        {
+            // Generate a value between 0 and 1, then scale to [-2, 2]
+            float randVal = static_cast<float>(game::fastRand(seed, 1001)) / 1000;
+            data_x_ptr[j] = randVal * 4.0f - 2.0f; // Scale and shift to [-2, 2]
+        }
+        data_y_ptr[0] = data_x_ptr[0] * m11 + data_x_ptr[1] * m12 + b1;
+        data_y_ptr[1] = data_x_ptr[0] * m21 + data_x_ptr[1] * m22 + b2;
+        std::vector<float> data_in = {data_x_ptr[0], data_x_ptr[1]};
+        float *result = nn.FeedForwardSingle(data_in.data());
+        REQUIRE(abs(result[0] - data_y_ptr[0]) < 0.02f);
+        REQUIRE(abs(result[1] - data_y_ptr[1]) < 0.02f);
+    }
+}
+
+TEST_CASE("Linear regression with 2 inputs layers and 2 outputs layers")
+{
+    Input input1(1);
+    Input input2(1);
+    Concatenate input({&input1, &input2});
+    Dense output1(&input, 1);
+    Dense output2(&input, 1);
+    Model nn({&input1, &input2}, {&output1, &output2});
+
+    nn.compile("Adam(learning_rate=0.2)");
+
+    const size_t data_samples = 10;
+    const size_t prod_samples = data_samples * data_samples;
+
+    float data_x[prod_samples * 2] = {0};
+    float data_y[prod_samples * 2] = {0};
+    float m11 = 1.5f;
+    float m12 = -1.5f;
+    float b1 = -1.0f;
+
+    float m21 = -2.5f;
+    float m22 = 0.5f;
+    float b2 = -0.5f;
+    for (size_t i = 0; i < data_samples; i++)
+    {
+        for (size_t j = 0; j < data_samples; j++)
+        {
+            int idx = (i * data_samples + j) * input.size_out;
+            data_x[idx] = (float)i - 1.0f;
+            data_x[idx + 1] = (float)1.0f - j;
+            data_y[idx] = data_x[idx] * m11 + data_x[idx + 1] * m12 + b1;
+            data_y[idx + 1] = data_x[idx] * m21 + data_x[idx + 1] * m22 + b2;
+        }
+    }
+
+    nn.fit(nn.weights, nn.weights, data_x, data_y, prod_samples, 100, 1);
+    for (size_t i = 0; i < nn.weights_size; i++)
+    {
+        printf("w[%lu]: %f\n", i, nn.weights[i]);
+    }
+
+    REQUIRE(abs(nn.weights[0] - m11) < 0.01f);
+    REQUIRE(abs(nn.weights[1] - m12) < 0.01f);
+    REQUIRE(abs(nn.weights[2] - b1) < 0.01f);
+
+    REQUIRE(abs(nn.weights[3] - m21) < 0.01f);
+    REQUIRE(abs(nn.weights[4] - m22) < 0.01f);
+    REQUIRE(abs(nn.weights[5] - b2) < 0.01f);
 }
